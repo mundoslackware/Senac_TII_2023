47ba0f868f11e73d8f20b378abf830900aaf2d41
Luciano Custodio <luciano@mundoslackware.com.br>
2023-03-14 15:25:32 -0300

    props

---
+++ 14-03/compon_props/src/App.js
@@ -0,0 +1,15 @@
+import Equipe from './components/equipe'
+
+function App() {
+  return (
+    <div>
+     <h1>Componentes e Props</h1>
+     <Equipe nome='Luciano' cargo='Programador' />
+     <Equipe nome='Marcos' cargo='Designer' />
+     <Equipe nome='João' cargo='DevOps' />
+     <Equipe nome='Camila' cargo='Gerente' />
+    </div>
+  )
+}
+
+export default App
---
+++ 14-03/compon_props/src/components/equipe/index.js
@@ -0,0 +1,13 @@
+
+
+function Equipe(props){
+    return(
+        <div>
+            <h2>Equipe:</h2><br />
+            <h3>Nome: {props.nome} - Cargo: {props.cargo}</h3>
+            <hr />
+        </div>
+    )
+}
+
+export default Equipe
